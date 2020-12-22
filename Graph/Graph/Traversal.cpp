f3a40e2bab5429ea0643f28c8baa7aa80499e4a8
Lê Hoàng Anh <lehoanganh.le2001@gmail.com>
2020-12-22 15:52:36 +0700

    Revert "update DFS (adjacency list)"
    This reverts commit c26c79abad6ac8a55b3eeb6f099608888e438a35.

--- Graph/Graph/Traversal.cpp
+++ Graph/Graph/Traversal.cpp
@@ -8,7 +8,7 @@
 #include "Traversal.h"
 
 void DFS(const vector<vector<bool> > &adj, vector<bool> &visited, int v) {
-    if (adj.size() != visited.size())
+    if (v < 1 || v > adj.size() || adj.size() != visited.size())
         return;
 
     if (!visited[v - 1]) {
@@ -22,30 +22,38 @@ void DFS(const vector<vector<bool> > &adj, vector<bool> &visited, int v) {
 }
 
 void DFS(const vector<list<int> > &adj, vector<bool> &visited, int v) {
-    if (adj.size() != visited.size())
+    if (v < 1 || v > adj.size() || adj.size() != visited.size())
         return;
 
-    stack<int> s;
+    stack<int> st;
 
+    st.push(v - 1);
     visited[v - 1] = true;
-    s.push(v - 1);
+    cout << v << "  ";
 
-    while (!s.empty()) {
-        int u = s.top();
-        cout << u + 1 << "  ";
+    while (!st.empty()) {
+        int u = st.top();
 
-        for (auto it = adj[u].begin(); it != adj[u].end(); it++)
-            if (!visited[*it]) {
+        list<int>::const_iterator it = adj[u].begin();
+        while (it != adj[u].end()) {
+            if (visited[*it])
+                ++it;
+            else {
+                cout << *it + 1 << "  ";
                 visited[*it] = true;
-                s.push(*it);
+                st.push(*it);
+                
+                u = *it;
+                it = adj[u].begin();
             }
+        }
 
-        s.pop();
+        st.pop();
     }
 }
 
 void BFS(const vector<vector<bool> > &adj, vector<bool> &visited, int v) {
-    if (adj.size() != visited.size())
+    if (v < 1 || v > adj.size() || adj.size() != visited.size())
         return;
 
     size_t countVertices = adj.size();
@@ -72,7 +80,7 @@ void BFS(const vector<vector<bool> > &adj, vector<bool> &visited, int v) {
 }
 
 void BFS(const vector<list<int> > &adj, vector<bool> &visited, int v) {
-    if (adj.size() != visited.size())
+    if (v < 1 || v > adj.size() || adj.size() != visited.size())
         return;
 
     queue<int> q;
